d32ff1490818d79b4008896319e0c72f64807b6f
funix <funix@funix.fritz.box>
2021-11-28 10:42:09 +0100

    gyro

---
+++ dictio.js
@@ -0,0 +1,76 @@
+var introintro = ['Werlcome to etheraxisisisisisisis']
+
+var coord = [ //lat, lng, n on paper map
+    [51.92498909590063, 4.474555854520307, "Station n. 1"],
+    [52.223829978037415, 6.891185448897737, "Station n. 8"],
+    [51.92428120851618, 4.475536640673075, "Station n. 3"],
+    [51.9244038691697, 4.472863218470833, "Station n. 14"],
+    [51.92428120851618, 4.475536640673075, "Station n. 0"]
+];
+
+var narration = [
+    'This is the text from sickhouse to smartshop',
+    'This is the text from smartshop to training station',
+    'This is the text from training to archive',
+    'This is the text from archive to shrooms',
+    'This is the text from shroom to sickhouse',
+]
+
+var qq = [
+    'Picture the perfect tool in your hand, is it electronic?',
+    'Would like being able to reach reach everything by foot?',
+    'If you collected an archive of your life, would it fit in the cloud or in your hands?',
+    'Have you ever felt the radio active static as part of your aura?',
+    'Will you follow the lightbeam on the horizon or will you turn and run to the woods?',
+];
+
+var item = [
+    '',
+    'gyro',
+    'archive',
+    'ble'
+]
+
+
+var quests = [
+    ['No', 'Yes'], //smartshop 1
+    ['Yes', 'No'], //training 2
+    ['in my pockets', 'in the cloud'], //water 3
+    ['yes', 'no'], //mushroom 4
+]
+
+var rid = [
+    'Sensor! The bridge point to your realm is a particular one, people seek it to change their perception. Inbetween flashing lights of many colours, you see a rotating text in a window display. What is the word that is written on it?',
+    'Sychronizer! Follow the metal fence alongside the contruction site. Right at its end, you see a red and white barrier. Go beyond it and around the building to reach the bridge point. Do you see the two yellow poles in front of the garage doors? Dare to lift the metal to its feet to pull out a little camoflaged container. The codeword is inside. Put it back as you found it after.',
+    'Energizer! The bridge point to your realm is protected by a hooded creature standing in wet matter. Close by, you will find a black metal fence. The codeword is inside a tiny magnetic capsule next to one of its screws. Put it back as you found it after',
+    'Transmitter! The bridge point to your realm is centered around a big tree on a crossing. At its feet, there is a lucious polypore fungus. Draw a line from this tree to the closest fence in your mind. This is where you will find a tiny magnetic container hidden next to a screw. Put it back as you found it after',
+    'Sensor! Sychronizer! Energizer! Transmitter! You have unlocked the four bridge points and are now able to reach the liminal space of EtherAxis! Return to the table, quick!'
+]
+
+var password = [
+    'immergo',
+    'revelare',
+    'supernatet',
+    'rete'
+]
+
+
+
+
+
+var instru = [
+    'You’re about to make contact with the realm of the sensor. Information excites us. Lots of it. With lights and sounds and smells and vibrations and words and feelings and explosions of nuances. But information also means power, which means responsibility. Information can be subtle, it doesn’t always choose to present itself in a decrypted way, so you need to become hyper aware and keep adapting yourself. Once you become one with the endless flows of information, you become fluid. It’s the ultimate goal, as transformation is the only constant in the emerging state of turbulence. Welcome aboard, what’s your preferred destination?',
+
+    'You’re about to make contact with the realm of the synchroniser. We like to feel the streams of energy through the movements of our physical being. Once you really learn to pay attention to the flow, you can feel the air bubbles in your blood rising from the bottom of your being to the ends of your limbs, and circling back into the earth. Not only this, but you’d feel the flow in others too. It’s exciting, isn’t it? As together, we are one flock. We’re a swarm. All moving in different paths but heading in the same direction. As we move our bodies and you move your bodies, we transmit the information that mind and matter hold. How about you give it a try? When you really concentrate, can you feel the vibration in your fingertips?',
+    'You’re about to make contact with the realm of the energizer. Imagine yourself on a train. Or any kind of transportation method with a view. Actually, imagine yourself being the train, the view is better that way. The tracks lead far into the unknown. You just keep going and have endless energy, while the sun goes up and down and up and down as you travel through light and darkness. It’s the constant change that keeps you going. You move so quick that the wind hurts your skin, but the speed is the only thing you have left. If you’d slow down or stop you would be blown away, so you just keep going and embrace the terrains of the unknown. You move so quick that outsiders in the Ether can’t even see the edges of your physical being. That’s us. We’re beams of light, constantly moving, adding to the knowledge streams while bypassing the hurdles of stagnation.',
+    'You’re about to make contact with the realm of the transmitter. Any being emits knowledge. It’s in the flow of the water, the thoughts of minds, migrations of bodies. Within the interconnectedness of energy streams there is no knowledge unknown. It’s vast and endless, really. We know how to connect our multiple bodies to that stream. Imagine an ant nest or beehive and how the individual bodies communicate with each other. Together they form one. We are not the beehive. Together, we are only a singular bee, responding to the other interconnected beings in the Ether. It’s only through making kin and adding new layers we get to overcome the changing of times.',
+    ''
+]
+
+var layers = [
+    'first',
+    'second',
+    'third',
+    'four',
+    'five'
+]
